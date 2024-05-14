7e5c0680b9058b26a73d19b2ae833eb9f0560c51
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-05-14 22:03:56 +0400

    Started with the second assignment
    TODO:
    Develop an IP-filtering program

---
+++ 02_IP_Sort/IP_Adress.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct IP_Adress
+{
+    int n1, n2, n3, n4;
+
+    void ReadAdress(int n1, int n2, int n3, int n4);    
+};
---
+++ 02_IP_Sort/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "IP_Adress.h"
+
+int main(){
+
+    IP_Adress t;
+
+    t.ReadAdress(1,1,1,1);    
+
+    return 0;
+}
