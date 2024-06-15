48237a088373aa1fff66edaee3dbed659190869d
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-06-15 23:41:41 +0400

    alloc traits try

--- 03_Allocator/allocator.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-class allocator_log{
-
-};
--- 03_Allocator/main.cpp
+++ 03_Allocator/main.cpp
@@ -1,8 +1,32 @@
+#include <memory>
+#include <map>
+#include <iostream>
+
+
+template <typename T> 
+struct allocator_log{
+
+    using value_type = T;
+
+    allocator_log();
+
+    template <typename U>
+    allocator_log(const allocator_log<U>&);
+
+    T* allocate(std::size_t n);
+
+    void deallocate(T* p, std::size_t n);
+
+};
 
 
 int main(){
 
+std::map <int, bool, std::less<int>, allocator_log<std::pair<int, bool>>> log_map;
 
+log_map.emplace(std::make_pair(2, true));
+
+std::cout << log_map.at(1);
 
     return 0;
 }
