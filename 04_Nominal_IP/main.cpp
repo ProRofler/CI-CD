7c6bf7164cc04d406937d2367dcf20d2574fa80b
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-07-10 23:10:56 +0400

    minor update

--- 04_Nominal_IP/main.cpp
+++ 04_Nominal_IP/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <type_traits>
 
-#define UNUSED(variable) (void)variable
+template<typename T> //integer fnunction
+typename std::enable_if<std::is_integral<T>::value, void>::type print_ip_adress(T value){
+
+
+
+    std::cout << "temp output" << std::endl;
+}
 
 int main(){
+
+    
 
     return 0;
 }
