0224da119bf63a7de56ccfda8a37561cc759b61f
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-07-19 14:08:51 +0400

    handled vector\list output

--- 04_Nominal_IP/main.cpp
+++ 04_Nominal_IP/main.cpp
@@ -57,7 +57,18 @@ constexpr bool are_same_type_tuple =
 // list vector metafunction
 template <typename T>
 typename std::enable_if<is_container<T>::value, void>::type print_ip(T value) {
-    std::cout << "List\\vector output template " << value.front() << std::endl;
+    std::cout << "Template output for vector/list: ";
+    auto it = value.begin();
+    for (const auto element : value) {
+        std::cout << element;
+
+        if (std::next(it) == value.end()) {
+            std::cout << std::endl;
+        } else {
+            std::cout << '.';
+        }
+        it++;
+    }
 }
 
 template <typename T>  // integer func
@@ -69,28 +80,28 @@ typename std::enable_if<std::is_integral<T>::value, void>::type print_ip(
 template <typename T>  // string func, output as is
 typename std::enable_if<std::is_same<T, std::string>::value>::type print_ip(
     T value) {
-    std::cout << "string output template " << value << std::endl;
+    std::cout << "Template output for string: " << value << std::endl;
 }
 
 template <typename T>
 typename std::enable_if<
     is_tuple<T>::value &&
         are_same_type_tuple<typename std::tuple_element<0, T>::type,
-                            typename std::tuple_element<1, T>::type,
-                            typename std::tuple_element<2, T>::type,
+                            // typename std::tuple_element<1, T>::type,
+                            // typename std::tuple_element<2, T>::type,
                             typename std::tuple_element<
                                 std::tuple_size<T>::value - 1, T>::type>,
     void>::type
 print_ip(const T& value) {
     std::cout << "tuple output template ";
-    std::apply([](const auto&... args) { ((std::cout << args << ' '), ...); },
+    std::apply([](const auto&... args) { ((std::cout << args << '.'), ...); },
                value);
     std::cout << std::endl;
 }
 
 // template <typename T>
-// typename std::enable_if<is_tuple<T>::value && are_same_type_tuple<typename
-// T::value_type>, void>::type print_ip(T) {
+// typename std::enable_if<is_tuple<T>::value &&
+// are_same_type_tuple<typename T::value_type>, void>::type print_ip(T) {
 //     std::cout << "tuple output template " << std::endl;
 // }
 
@@ -102,16 +113,16 @@ print_ip(const T& value) {
 // TODO: fold expression for outputs
 
 int main() {
-    print_ip(int8_t{-1});           // 255
-    print_ip(int16_t{0});           // 0.0
-    print_ip(int32_t{2130706433});  // 127.0.0.1
-    print_ip(int64_t{8875824491850138409});
+    // print_ip(int8_t{-1});           // 255
+    // print_ip(int16_t{0});           // 0.0
+    // print_ip(int32_t{2130706433});  // 127.0.0.1
+    // print_ip(int64_t{8875824491850138409});
 
     print_ip(std::string{"Hello, World!"});
 
     print_ip(std::vector<int>{100, 200, 300, 400});  // 100.200.300.400
     print_ip(std::list<short>{400, 300, 200, 100});  // 400.300.200.100
-    print_ip(std::make_tuple(123, 456, 789, 0));  // 123.456.789.0
+    // print_ip(std::make_tuple(123, 456, 789, 0));  // 123.456.789.0
 
     return 0;
 }
