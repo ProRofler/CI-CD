f1ccf354d0f87338103c1f8deaed2307fe96f9c5
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-08-19 21:32:28 +0400

    unused var fix

--- 05_Matrix/main.cpp
+++ 05_Matrix/main.cpp
@@ -11,14 +11,21 @@ int main() {
     assert(matrix.size() == 0);
 
     auto a = matrix[2][3];
+    std::cout << a << '\n';
     assert(a != -1);
-    //assert(matrix.size() == 0);
+    // assert(matrix.size() == 0);
 
     matrix[100][100] = 314;
     assert(matrix[100][100] == 314);
-    //assert(matrix.size() == 2);
+    // assert(matrix.size() == 2);
 
-    for (auto a : matrix){
-        std::cout << a <<'\n';
+    for (int i = 0; i < 10; i++) {
+        for (int j = 9; j > 0; j--) {
+            matrix[i][j] = j - i;
+        }
+    }
+
+    for (auto c : matrix) {
+        std::cout << c << '\n';
     }
 }
