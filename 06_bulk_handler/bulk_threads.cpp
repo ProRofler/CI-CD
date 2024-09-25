84bdecb41834b296dfcfe6e7bae37651ad58dca6
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-09-25 21:01:46 +0400

    fix so it builds

--- 06_bulk_handler/bulk_threads.cpp
+++ 06_bulk_handler/bulk_threads.cpp
@@ -1,7 +1,12 @@
 #include "bulk_threads.h"
 
+#define UNUSED(variable) (void)variable
+
 void bulk_threads::run_in_log_thread(std::function<void()> task) {
-    log(task);
-    log.join();
+    UNUSED(task);
+    //log(task);
+    //log.join();
 }
-void bulk_threads::run_in_file_thread(std::function<void()> task) {}
+void bulk_threads::run_in_file_thread(std::function<void()> task) {
+    UNUSED(task);
+}
