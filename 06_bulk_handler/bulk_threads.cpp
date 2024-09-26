98e44d50f04e1988ea73092e579d90fc565ff191
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-09-26 16:46:24 +0400

    updates on thread class

--- 06_bulk_handler/bulk_threads.cpp
+++ 06_bulk_handler/bulk_threads.cpp
@@ -4,8 +4,6 @@
 
 void bulk_threads::run_in_log_thread(std::function<void()> task) {
     UNUSED(task);
-    //log(task);
-    //log.join();
 }
 void bulk_threads::run_in_file_thread(std::function<void()> task) {
     UNUSED(task);
--- 06_bulk_handler/bulk_threads.h
+++ 06_bulk_handler/bulk_threads.h
@@ -1,15 +1,39 @@
 #pragma once
 
+#include <condition_variable>
 #include <functional>
+#include <mutex>
+#include <queue>
 #include <thread>
 
 class bulk_threads {
    public:
+    bulk_threads() : stop(false) {
+        log_thread = std::thread(&bulk_threads::log_tasks_control, this);
+        file_1_thread = std::thread(&bulk_threads::file_tasks_control, this);
+        file_2_thread = std::thread(&bulk_threads::file_tasks_control, this);
+    }
+
+    ~bulk_threads() {
+        if (log_thread.joinable()) log_thread.join();
+        if (file_1_thread.joinable()) file_1_thread.join();
+        if (file_2_thread.joinable()) file_2_thread.join();
+    }
+
     void run_in_log_thread(std::function<void()> task);
     void run_in_file_thread(std::function<void()> task);
 
    private:
-    std::thread log, file_1, file_2;
+    void log_tasks_control();
+    void file_tasks_control();
 
-    static size_t counter;
+    std::thread log_thread, file_1_thread, file_2_thread;
+
+    std::queue<std::function<void()>> file_tasks;
+    std::queue<std::function<void()>> log_tasks;
+
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+
+    bool stop;
 };
